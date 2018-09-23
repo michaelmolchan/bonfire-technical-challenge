2ea168cc92c477c5145e963b062532c601dd411a
michaelmolchan <mmolchan91@gmail.com>
2018-09-23 19:45:23 -0400

    Add sections for buttons and boxes

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -23,10 +23,76 @@ $(() => {
     }
   })
 
-  // closes mobile menu when a link is clicked.
+  // closes mobile menu when a link is clicked
   $('#menu').click(() => {
     toggleIcon.className = 'menuIcon'
     nav.style.transform = 'translateX(-100%)'
     nav.style.transition = 'transform 0.2s ease-out'
+  })
+
+  $('#show-all').focus()
+
+  $('#show-all').click(() => {
+    $('.even').show()
+    $('.odd').show()
+  })
+
+  $('#show-odd').click(() => {
+    $('.even').hide()
+    $('.odd').show()
+  })
+
+  $('#show-even').click(() => {
+    $('.odd').hide()
+    $('.even').show()
+  })
+
+  // Hide/show box buttons
+  $('.box-one').on('mouseenter', function () {
+    $('#btn-1').delay(50).fadeIn(100)
+  })
+
+  $('.box-one').on('mouseleave', function () {
+    $('#btn-1').delay(50).fadeOut(100)
+  })
+
+  $('.box-two').on('mouseenter', function () {
+    $('#btn-2').delay(50).fadeIn(100)
+  })
+
+  $('.box-two').on('mouseleave', function () {
+    $('#btn-2').delay(50).fadeOut(100)
+  })
+
+  $('.box-three').on('mouseenter', function () {
+    $('#btn-3').delay(50).fadeIn(100)
+  })
+
+  $('.box-three').on('mouseleave', function () {
+    $('#btn-3').delay(50).fadeOut(100)
+  })
+
+  $('.box-four').on('mouseenter', function () {
+    $('#btn-4').delay(50).fadeIn(100)
+  })
+
+  $('.box-four').on('mouseleave', function () {
+    $('#btn-4').delay(50).fadeOut(100)
+  })
+
+  $('.box-five').on('mouseenter', function () {
+    $('#btn-5').delay(50).fadeIn(100)
+  })
+
+  $('.box-five').on('mouseleave', function () {
+    $('#btn-5').delay(50).fadeOut(100)
+  })
+
+  $('.box-six').on('mouseenter', function () {
+    $('#btn-6').delay(50).fadeIn(100)
+  })
+
+  $('.box-six').on('mouseleave', function () {
+    $('#btn-6').delay(50).fadeOut(100)
   })
 })
